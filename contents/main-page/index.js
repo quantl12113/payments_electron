9f6331a7870efc6cef6703a40c0e5567e778cf0e
kien.do <kien.do@vietis.com.vn>
2019-09-18 15:51:05 +0700

    add mail tab, other tab

--- contents/main-page/index.js
+++ contents/main-page/index.js
@@ -1,38 +1,82 @@
 const fs = require('fs');
 const path = require('path');
-const $ = require( 'jquery' );
-require( 'jszip' );
-require( 'pdfmake' );
-require( 'datatables.net-zf' )(window, $);
-require( 'datatables.net-autofill-zf' )(window, $);
-require( 'datatables.net-buttons-zf' )(window, $);
-require( 'datatables.net-buttons/js/buttons.colVis.js' )(window, $);
-require( 'datatables.net-buttons/js/buttons.flash.js' )(window, $);
-require( 'datatables.net-buttons/js/buttons.html5.js' )(window, $);
-require( 'datatables.net-buttons/js/buttons.print.js' )(window, $);
-require( 'datatables.net-colreorder-zf' )(window, $);
-require( 'datatables.net-fixedcolumns-zf' )(window, $);
-require( 'datatables.net-fixedheader-zf' )(window, $);
-require( 'datatables.net-keytable-zf' )(window, $);
-require( 'datatables.net-responsive-zf' )(window, $);
-require( 'datatables.net-rowgroup-zf' )(window, $);
-require( 'datatables.net-rowreorder-zf' )(window, $);
-require( 'datatables.net-scroller-zf' )(window, $);
-require( 'datatables.net-select-zf' )(window, $);
+const $ = require('jquery');
+require('jszip');
+require('pdfmake');
+require('datatables.net-zf')(window, $);
+require('datatables.net-autofill-zf')(window, $);
+require('datatables.net-buttons-zf')(window, $);
+require('datatables.net-buttons/js/buttons.colVis.js')(window, $);
+require('datatables.net-buttons/js/buttons.flash.js')(window, $);
+require('datatables.net-buttons/js/buttons.html5.js')(window, $);
+require('datatables.net-buttons/js/buttons.print.js')(window, $);
+require('datatables.net-colreorder-zf')(window, $);
+require('datatables.net-fixedcolumns-zf')(window, $);
+require('datatables.net-fixedheader-zf')(window, $);
+require('datatables.net-keytable-zf')(window, $);
+require('datatables.net-responsive-zf')(window, $);
+require('datatables.net-rowgroup-zf')(window, $);
+require('datatables.net-rowreorder-zf')(window, $);
+require('datatables.net-scroller-zf')(window, $);
+require('datatables.net-select-zf')(window, $);
 
 
-let rawdata = fs.readFileSync(path.join(__dirname, 'data.json'));
-let data = JSON.parse(rawdata);
-console.log(data);
-$(document).ready( function () {
+let empRawdata = fs.readFileSync(path.join(__dirname, 'emp_data.json'));
+let empData = JSON.parse(empRawdata);
+let otherRawdata = fs.readFileSync(path.join(__dirname, 'other_data.json'));
+let otherData = JSON.parse(otherRawdata);
+$(document).ready(function () {
   $('#employee').DataTable({
-    data: data,
-    "bProcessing" : true,
-    "bDestroy" : true,
-    "bAutoWidth" : true,
-    "sScrollY" : "500px",
-    "bScrollCollapse" : true,
-    "bSort" : true,
-    "sPaginationType" : "full_numbers",
+    data: empData,
+    "bProcessing": true,
+    "bDestroy": true,
+    "bAutoWidth": true,
+    "sScrollY": "500px",
+    "bScrollCollapse": true,
+    "bSort": true,
+    "sPaginationType": "full_numbers",
   });
-} );
+
+  var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+
+  $('#other').DataTable({
+    data: otherData,
+    "bProcessing": true,
+    "bDestroy": true,
+    "bAutoWidth": true,
+    "sScrollY": "500px",
+    "bScrollCollapse": true,
+    "bSort": true,
+    "sPaginationType": "full_numbers",
+  });
+
+  $('#other_wrapper').css('display', 'none');
+  $('.ql-toolbar').css('display', 'none');
+  $('#editor').css('display', 'none');
+
+  $('#cssmenu>ul>li>').click((e) => {
+    $('.active').removeClass('active');
+    $(e.target).parent().addClass('active');
+    if ($(e.target).parent().attr('id') === 'emp-tab') {
+      $('.ql-toolbar').css('display', 'none');
+      $('#editor').css('display', 'none');
+      $('#other_wrapper').css('display', 'none');
+      $('#employee_wrapper').css('display', 'block');
+    }
+    if ($(e.target).parent().attr('id') === 'mail-tab') {
+      $('.ql-toolbar').css('display', 'block');
+      $('#editor').css('display', 'block');
+      $('#employee_wrapper').css('display', 'none');
+      $('#other_wrapper').css('display', 'none');
+    }
+    if ($(e.target).parent().attr('id') === 'other-tab') {
+      $('.ql-toolbar').css('display', 'none');
+      $('#editor').css('display', 'none');
+      $('#employee_wrapper').css('display', 'none');
+      $('#other_wrapper').css('display', 'block');
+    }
+  })
+  
+});
