788037f2497123aee0cb5486f85c4d5fd009c0b6
Le Anh Quan <quan.leanh@vietis.vn>
2019-10-14 14:26:16 +0700

    Implement code for add employee

--- contents/emp-create-form/index.js
+++ contents/emp-create-form/index.js
@@ -1,5 +1,25 @@
+const {
+  remote
+} = require('electron');
+const {
+  dialog
+} = remote;
+const {
+  BrowserWindow
+} = remote;
+const fs = require('fs');
+const path = require('path');
+const empModel = remote.require('./lib/employee-model');
+const otherModel = remote.require('./lib/other-model');
 const ipc = require('electron').ipcRenderer;
 
 $('#create-employ-submit').click(async () => {
-  ipc.sendSync('create-employee','1111111111111', '1234');
+  const employ = {};
+  employ.code = $("#employee-code").val();
+  employ.name = $("#employee-name").val();
+  employ.email = $("#employee-email").val();
+  employ.date = $("#employee-date").val();
+  employ.nameJapan = $("#employee-name-jp").val();
+  empModel.addEmployee(employ);
+  // ipc.sendSync('create-employee','1111111111111', '1234');
 });
